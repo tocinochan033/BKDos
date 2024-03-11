7fae0dd96ac3397356e781baf4b2404c60e3dbde
tocinochan033 <vazquezlmartin33@gmail.com>
2024-03-10 21:08:49 -0700

    Roles Identificador del rol cada inicio de sesion. Nuevo datagridView para datos de administradores

--- Proyecto_AdministracionOrgDatos/fmrRegistroAdmin.cs
+++ Proyecto_AdministracionOrgDatos/fmrRegistroAdmin.cs
@@ -16,7 +16,7 @@ using System.Media;
     El rol de 'obrera' tiene la misma funcionalidad de hasta ahora, no existe algun cambio alguno.
     El rol de 'mama oso' tambien posee la misma estrucutra, con la pecularidad de crear nuevos administradores
     'obrera'. Este rol tambien permite poder borrar a los administradores con etiqueta 'obrero' con confirmacion
-    de su contraseña.
+    de su contraseña (NO IMPLEMENTADO).
 */
 
 //-------------------------------------------------------------------------------------------------------------//
@@ -28,12 +28,13 @@ namespace Proyecto_AdministracionOrgDatos
 {
     public partial class fmrRegistroAdmin : Form
     {
-        FileStream login = new FileStream("login.txt", FileMode.Append, FileAccess.Write);
+        //crea aleatoriamente numeros de 6 digitos para la contraseña
         Random rnd = new Random();
 
         public fmrRegistroAdmin()
         {
             InitializeComponent();
+            cargaDatos();
         }
 
         private void fmrRegistroAdmin_Load(object sender, EventArgs e)
@@ -49,21 +50,25 @@ namespace Proyecto_AdministracionOrgDatos
             rolCombo.Text  = null;
         }
 
-        public void guardar()
+        public void guardar() //campo para guarda los datos dentro de una linea nuevo del archivo
         {
-            using(StreamWriter writer = new StreamWriter(login))
+            using(FileStream login = new FileStream("login.txt", FileMode.Append, FileAccess.Write))
             {
-                //Nombre,Correo, Numero, Rol, contraseña;
-                writer.WriteLine($"{nombreTxt.Text.ToLower()},{correoTxt.Text.ToLower()},{numeroTxt.Text},{rolCombo.Text},{rnd.Next(9999999)}");
+                using (StreamWriter writer = new StreamWriter(login))
+                {
+                    //nombre,correo,numero,rol,contraseña
+                    writer.WriteLine($"{nombreTxt.Text.ToLower()},{correoTxt.Text.ToLower()},{numeroTxt.Text},{rolCombo.Text},{rnd.Next(9999999)}");
+                }
+                login.Close();
             }
         }
 
         private void newAdminButton_Click(object sender, EventArgs e)
         {
             SystemSounds.Exclamation.Play();
-            string confirmacion = Interaction.InputBox("Favor de confirmar contraseña", "Contraseña");
+            string confirmacion = Interaction.InputBox("Favor de confirmar contraseña", "Contraseña"); //messageBox con textbos incluido. Confirma contraseña
 
-            if(int.Parse(confirmacion) == 2)
+            if(int.Parse(confirmacion) == 2) //Cada nuevo administrador requiere confirmacion de contraseña: 2
             {
                 guardar();
                 camposLimpieza();
@@ -73,6 +78,39 @@ namespace Proyecto_AdministracionOrgDatos
             {
                 MessageBox.Show("Favor de no olvidar todos los datos en casa.", "Datos incompletos", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
+            //login.Close();
+        }
+
+        public void cargaDatos()
+        {
+            int indiceNuevoRenglon;
+            string nombre, correo, numero, rol;
+
+            using (FileStream loginStream = new FileStream("login.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader lector = new StreamReader(loginStream))
+                {
+                    string renglon;
+                    while ((renglon = lector.ReadLine()) != null)
+                    {
+                        string[] datos = renglon.Split(',');
+
+                        indiceNuevoRenglon = administradoresDataGrid.Rows.Add();
+
+                        nombre = datos[0];
+                        correo = datos[1];
+                        rol = datos[2];
+                        numero = datos[3];
+
+                        administradoresDataGrid.Rows[indiceNuevoRenglon].Cells[0].Value = nombre;
+                        administradoresDataGrid.Rows[indiceNuevoRenglon].Cells[1].Value = correo;
+                        administradoresDataGrid.Rows[indiceNuevoRenglon].Cells[2].Value = rol;
+                        administradoresDataGrid.Rows[indiceNuevoRenglon].Cells[3].Value = numero;
+                    }
+                    loginStream.Close();
+                }
+            }
         }
     }
 }
+
