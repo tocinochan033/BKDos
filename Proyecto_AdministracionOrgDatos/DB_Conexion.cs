712799fdc213caa666d9db109f310db530296bde
david estrada <abrahamestradasolano@gmail.com>
2024-05-01 22:20:01 -0700

    creacion de clase

---
+++ Proyecto_AdministracionOrgDatos/DB_Conexion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_AdministracionOrgDatos
+{
+    internal class DB_Conexion
+    {
+        private static string Conexion = @"server=pc\DESKTOP-EOG5OVI; Initial Catalog = BKDOS; integrated security=true";
+
+        public static SqlConnection Conectar()
+        {
+            SqlConnection con = new SqlConnection(Conexion);
+
+            //Valida el estado de la conexion
+            if(con.State != System.Data.ConnectionState.Open)
+            {
+                con.Open();
+            }
+            return con;
+        }
+    }
+}
